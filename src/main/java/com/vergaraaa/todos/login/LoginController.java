247629c1ae6eb10884c6d019d4718baa21dab7cb
Edgar Vergara <edgarvm13@hotmail.com>
2023-11-03 08:49:55 +0100

    displaying login credentials in a jsp using model

--- src/main/java/com/vergaraaa/todos/login/LoginController.java
+++ src/main/java/com/vergaraaa/todos/login/LoginController.java
@@ -3,15 +3,27 @@ package com.vergaraaa.todos.login;
 // import org.slf4j.Logger;
 // import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
     // private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("login")
+    @RequestMapping(value = "login", method = RequestMethod.GET)
     public String goToLoginPage() {
 
         return "login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+
+        model.put("name", name);
+        model.put("password", password);
+
+        return "welcome";
     }
 }
